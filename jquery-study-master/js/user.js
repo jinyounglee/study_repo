279ccb177e7fd4f6e59bacb43e58e703fc201437
jinyounglee <hs25823@naver.com>
2015-09-05 11:54:27 +0900

    commit

--- jquery-study-master/js/user.js
+++ jquery-study-master/js/user.js
@@ -20,6 +20,9 @@ var user = {
 		this.$el.find('#btnClose').click(function(){
 			user.closeModal();
 		});	
+		this.$el.find('#btnSubmit').click(function(){
+			user.signUp();
+		});
 	},
 	showModal : function(){
 		this.resetModal();
@@ -28,8 +31,49 @@ var user = {
 	},
 	closeModal : function(){
 		this.$el.find('#userModal').modal('hide');		
-	}
+	},
 	resetModal : function(){
 		this.$el.find('.signForms').val('');
+	},
+	addClass : function(){
+		this.$el.find('#memberModal input').addClass( "empty" );
+	},
+	removeClass : function(){
+		this.$el.find('#memberModal input').removeClass("class", "empty");
+	},
+	signUp : function(){
+		var email = this.$el.find('#inputEmail').val(),
+		password = this.$el.find('#inputPassword').val(),
+		passwordConfirm = this.$el.find('#passwordConfirm').val(),
+		name = this.$el.find('#inputName').val(),
+		job = this.$el.find('#inputJob').val();
+
+
+	// 입력창에 빈칸은 없는가?
+	// 빈칸이 있으면 해당 입력칸에 강조효과 (empty 클래스 추가 )주기
+	// 저장 실패 alert
+	// 2. password 와 passwordConfirm이 같은가? 다르면 패스워드 확인 all
+	// 3. 이미 등록되 사용자가 아닌가?
+	// 4. 위 검증이 끝나면 회원 가입 
+		if( email == null ){
+			alert('email 을 입력하세요 ');
+			this.$el.find('#inputEmail').val('').focus().addClass("empty");
+		} else if ( password  == null ){
+			alert('password를 입력하세요 ');
+			this.$el.find('#inputPassword').addClass("empty").val('').focus();
+		} else if ( passwordConfirm  == null ){
+			alert('password를 입력하세요 ');
+			this.$el.find('#inputPasswordConfirm').val('').focus().addClass("empty");
+		} else if ( name  == null ){
+			alert('이름을 입력하세요 ');
+			this.$el.find('#inputName').val('').focus().addClass("empty");
+		} else if ( name == null ){
+			alert('직업을 입력하세요 ');
+			this.$el.find('#inputJob').val('').focus().addClass("empty");
+		} else if( password != passwordConfirm ){
+			alert("비밀번호확인이 맞지 않습니다 다시 입력해주세요 ");
+			password.val('').focus().addClass("empty");
+		}	
+
 	}
 }
