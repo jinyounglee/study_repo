c894b0775405146ec36c992dd9367b950335c57e
jinyounglee <hs25823@naver.com>
2015-09-12 10:23:24 +0900

    find 함수 생성

--- jquery-study-master/js/user.js
+++ jquery-study-master/js/user.js
@@ -9,6 +9,30 @@ $(function(){
 	user.init();
 });
 
+// db연동시 사라짐
+var users = [];
+var currentTime = new Date();
+// 굳이 변수에 담아서 메모리 할당 할 필요가 없다. 
+
+var users = [
+	{
+		email : '1',
+		password : '2',
+		name : '1',
+		job : '1',
+		joinDate : currentTime,
+		updateDate : currentTime
+	},
+	{
+		email : '2',
+		password : '2',
+		name : '2',
+		job : '2',
+		joinDate : currentTime,
+		updateDate : currentTime
+}];
+
+
 var user = {
 	$el : {},
 
@@ -48,32 +72,53 @@ var user = {
 		name = this.$el.find('#inputName').val(),
 		job = this.$el.find('#inputJob').val();
 
+		this.find(email);	
 
 	// 입력창에 빈칸은 없는가?
 	// 빈칸이 있으면 해당 입력칸에 강조효과 (empty 클래스 추가 )주기
 	// 저장 실패 alert
 	// 2. password 와 passwordConfirm이 같은가? 다르면 패스워드 확인 all
 	// 3. 이미 등록되 사용자가 아닌가?
+	// find 함수를 만들고 email이 중복인지 체크해서 
 	// 4. 위 검증이 끝나면 회원 가입 
-		if( email == null ){
-			alert('email 을 입력하세요 ');
-			this.$el.find('#inputEmail').val('').focus().addClass("empty");
-		} else if ( password  == null ){
-			alert('password를 입력하세요 ');
-			this.$el.find('#inputPassword').addClass("empty").val('').focus();
-		} else if ( passwordConfirm  == null ){
-			alert('password를 입력하세요 ');
-			this.$el.find('#inputPasswordConfirm').val('').focus().addClass("empty");
-		} else if ( name  == null ){
-			alert('이름을 입력하세요 ');
-			this.$el.find('#inputName').val('').focus().addClass("empty");
-		} else if ( name == null ){
-			alert('직업을 입력하세요 ');
-			this.$el.find('#inputJob').val('').focus().addClass("empty");
-		} else if( password != passwordConfirm ){
-			alert("비밀번호확인이 맞지 않습니다 다시 입력해주세요 ");
-			password.val('').focus().addClass("empty");
-		}	
 
+
+	},
+
+	validate : function(){
+		var $signForms = this.$el.find('.signForms'),
+			result = true;
+
+		$.each($signForms, function(index, signForm){
+			var $signForm = $(signForm);
+
+			if(!$signForm.val()){
+				$signForm.addClass('empty');
+				result = false;
+			}else{
+				$signForm.removeClass('empty');
+			}
+		});
+		return result;
+	},
+/*
+	find : function(email){
+		for( var i=0; i< users.length; i++){
+			if( email == users[i].email ){
+				alert('중복된 사용자입니다.');
+				return false;
+			}
+		}
+	}*/
+
+	find : function(email){
+		$.each(users, function(index, value){
+			var userTemp = value;
+			if( userTemp.email === email ){
+				result = true;
+				return;
+			}	
+		});
+		return result;
 	}
 }
